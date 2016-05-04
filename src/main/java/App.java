79dcc6b4abbfa91bc1845c0a487465e1357e1aec
ming wen and scott mcintire <scottmcintire90@gmail.com>
2016-05-04 10:56:07 -0700

    add css

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,67 +10,68 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-  //   get("/", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("template", "templates/index.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/doctors/new", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("template", "templates/new-doctor-form.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/patients/new", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("doctors", Doctor.all());
-  //     model.put("template", "templates/new-patient-form.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   post("/doctors", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     String doctorName = request.queryParams("doctorName");
-  //     String specialty = request.queryParams("specialty");
-  //
-  //     Doctor newDoctor = new Doctor(doctorName, specialty);
-  //     newDoctor.save();
-  //     model.put("template", "templates/doctor-success.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/doctors", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("doctors", Doctor.all());
-  //     model.put("template", "templates/doctors.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   post("/patients", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     String patientName = request.queryParams("patientName");
-  //     String birthdate = request.queryParams("birthdate");
-  //     int doctor = Integer.parseInt(request.queryParams("doctor"));
-  //     Patient newPatient = new Patient(patientName, birthdate, doctor);
-  //     newPatient.save();
-  //     model.put("template", "templates/patient-success.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/patients", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     model.put("patients", Patient.all());
-  //     model.put("template", "templates/patients.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/doctors/:id", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap<String, Object>();
-  //     Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
-  //     model.put("doctor", doctor);
-  //     model.put("template", "templates/doctor.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/restaurants/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-restaurant-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // get("/reviews/new", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   model.put("restaurants", Restauanr.all());
+    //   model.put("template", "templates/new-patient-form.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+
+    post("/restaurants", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String restaurantName = request.queryParams("restaurantName");
+      String cuisineType = request.queryParams("cuisineType");
+      String city = request.queryParams("city");
+
+      Restaurant newRestaurant = new Restaurant(restaurantName, cuisineType, city);
+      newRestaurant.save();
+      model.put("template", "templates/restaurant-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/restaurants", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("restaurants", Restaurant.all());
+      model.put("template", "templates/restaurants.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // post("/patients", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   String patientName = request.queryParams("patientName");
+    //   String birthdate = request.queryParams("birthdate");
+    //   int restaurant = Integer.parseInt(request.queryParams("doctor"));
+    //   Patient newPatient = new Patient(patientName, birthdate, doctor);
+    //   newPatient.save();
+    //   model.put("template", "templates/patient-success.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // get("/patients", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   model.put("patients", Patient.all());
+    //   model.put("template", "templates/patients.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+    //
+    // get("/restaurants/:id", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
+    //   model.put("doctor", doctor);
+    //   model.put("template", "templates/doctor.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,44 +8,42 @@ import java.util.List;
 import org.sql2o.*;
 
 public class AppTest extends FluentTest {
-  // public WebDriver webDriver = new HtmlUnitDriver();
-  // @Before
-  // public void setUp() {
-  //   DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/office", null, null);
-  // }
-  //
-  // @After
-  // public void tearDown() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String deletePatientsQuery = "DELETE FROM patients *;";
-  //     String deleteDoctorsQuery = "DELETE FROM doctors *;";
-  //     con.createQuery(deletePatientsQuery).executeUpdate();
-  //     con.createQuery(deleteDoctorsQuery).executeUpdate();
-  //   }
-  // }
-  //
-  // @Override
-  // public WebDriver getDefaultDriver() {
-  //   return webDriver;
-  // }
-  //
-  // @ClassRule
-  // public static ServerRule server = new ServerRule();
-  //
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Welcome");
-  // }
-  // @Test
-  // public void doctorIsCreatedTest(){
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Add a Doctor"));
-  //   fill("#doctorName").with("John Smith");
-  //   fill("#specialty").with("Endocrinologist");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Your doctor has been saved.");
-  // }
+  public WebDriver webDriver = new HtmlUnitDriver();
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/restaurant", null, null);
+  }
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteReviewQuery = "DELETE FROM reviews *;";
+      String deleteRestaurantQuery = "DELETE FROM restaurants *;";
+      con.createQuery(deleteReviewQuery).executeUpdate();
+      con.createQuery(deleteRestaurantQuery).executeUpdate();
+    }
+  }
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Restaurant");
+  }
+  @Test
+  public void restaurantIsCreatedTest(){
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a Restaurant"));
+    fill("#restaurantName").with("Matador");
+    fill("#cuisineType").with("Mexican");
+    fill("#city").with("Portland");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your restaurant has been saved.");
+  }
   // @Test
   // public void doctorIsSavedTest(){
   //   Doctor myDoctor = new Doctor("John Smith", "Endocrinologist");
