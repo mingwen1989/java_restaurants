aadffd723ee8da1d11a3eebbf7b9c8227f151939
ming wen and scott mcintire <mwen82@gmail.com>
2016-05-04 11:51:05 -0700

    add style

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,7 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("restaurants", Restaurant.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -22,14 +23,31 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // get("/reviews/new", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("restaurants", Restauanr.all());
-    //   model.put("template", "templates/new-patient-form.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/restaurants/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Restaurant restaurant = Restaurant.find(Integer.parseInt(request.params(":id")));
+      model.put("restaurant", restaurant);
+      model.put("template", "templates/restaurant.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-    post("/restaurants", (request, response) -> {
+    get("/restaurants/:id/reviews", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+
+
+      model.put("template", "templates/new-patient-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/restaurants/:id/reviews/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("restaurants", Restaurant.all());
+      model.put("template", "templates/new-patient-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String restaurantName = request.queryParams("restaurantName");
       String cuisineType = request.queryParams("cuisineType");
@@ -40,38 +58,5 @@ public class App {
       model.put("template", "templates/restaurant-success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    get("/restaurants", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("restaurants", Restaurant.all());
-      model.put("template", "templates/restaurants.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    // post("/patients", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   String patientName = request.queryParams("patientName");
-    //   String birthdate = request.queryParams("birthdate");
-    //   int restaurant = Integer.parseInt(request.queryParams("doctor"));
-    //   Patient newPatient = new Patient(patientName, birthdate, doctor);
-    //   newPatient.save();
-    //   model.put("template", "templates/patient-success.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/patients", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("patients", Patient.all());
-    //   model.put("template", "templates/patients.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/restaurants/:id", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Doctor doctor = Doctor.find(Integer.parseInt(request.params(":id")));
-    //   model.put("doctor", doctor);
-    //   model.put("template", "templates/doctor.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
   }
 }
